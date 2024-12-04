45d8529e400f6bdb00f6a979284c32aa9a096a88
gavanv <gavan777@gmail.com>
2024-12-04 10:51:03 +0000

    finish ex2

--- HW1-files/ex2.asm
+++ HW1-files/ex2.asm
@@ -3,23 +3,22 @@
 .section .data
 
 MyArray:
-    .byte 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90
+    .byte 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0x91, 0x92, 0x93
 
 MyArrayResult:
-    .byte 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+    .byte 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
 
 Adrress: 
 	.quad MyArray
 
 Type: 
-	.byte 3
+	.int 4
 
 Length:
     .int 3
 
 LittleEndianResult:
 	.quad MyArrayResult
-
 
 
 .section .text
@@ -46,13 +45,13 @@ process_elements_HW1:
     leaq 0(%rsi), %rax           # Load the address of the current element
 
 reverse_bytes_HW1:
-    movb (%rax), %al             # Load a byte from the source
+    movb (%rax), %r8b             # Load a byte from the source
     
     # rdi - the destination starting address
     # rdx - the Type (size in bytes)
-    movb %al, -1(%rdi,%rdx, 1)     # Store it in reverse order in the destination
-    addq $1, %rax                # Move to the next byte in the source
     subq $1, %rdx                # Move to the previous byte in the destination
+    movb %r8b, (%rdi,%rdx, 1)     # Store it in reverse order in the destination
+    addq $1, %rax                # Move to the next byte in the source
     testq %rdx, %rdx             # Check if we reversed all bytes
     jnz reverse_bytes_HW1            # If not, continue reversing
 
