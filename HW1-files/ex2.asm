e04bf032863e2d025249c938ba7e6b579a5a48b0
gavanv <gavan777@gmail.com>
2024-12-04 19:17:33 +0000

    finish ex2

--- HW1-files/ex2.asm
+++ HW1-files/ex2.asm
@@ -2,42 +2,47 @@
 
 .section .text
 _start:
-    
-    # Load the base address of the source array
+    # Load the base address of the source
     movq (Adrress), %rsi
+    testq %rsi, %rsi           # Check if the address is 0
+    jz done_HW1                # If the address is 0, jump to done 
 
-    # Load the base address of the destination array
+    # Load the base address of the destination 
     movq (LittleEndianResult), %rdi
+    testq %rdi, %rdi           # Check if the address is 0
+    jz done_HW1                # If the address is 0, jump to done 
 
-    # Load the type (size of each element) into %ecx
-    movzbq Type(%rip), %rcx
+    # Load the type into %ecx
+    movzbq (Type), %rcx
 
-    # Load the length of the array into %ebx
-    movl Length(%rip), %ebx
+    # Load the length into %ebx
+    movl (Length), %ebx
 
-process_elements_HW1:
+main_loop_HW1:
     testl %ebx, %ebx             # Check if length is 0
-    jz done_HW1                      # If 0, we're done
+    jz done_HW1                  # If 0, we're done main loop
 
     # Reverse the bytes of the current element
-    movl %ecx, %edx              # Copy type (number of bytes) to %edx
-    leaq 0(%rsi), %rax           # Load the address of the current element
+    movl %ecx, %edx              # Copy type to %edx
+    
+    # Load the address of the current element
+    movq %rsi, %rax
 
 reverse_bytes_HW1:
     movb (%rax), %r8b             # Load a byte from the source
     
     # rdi - the destination starting address
     # rdx - the Type (size in bytes)
-    subq $1, %rdx                # Move to the previous byte in the destination
+    decq %rdx                     # Move to the previous byte in the destination
     movb %r8b, (%rdi,%rdx, 1)     # Store it in reverse order in the destination
-    addq $1, %rax                # Move to the next byte in the source
-    testq %rdx, %rdx             # Check if we reversed all bytes
-    jnz reverse_bytes_HW1            # If not, continue reversing
+    incq %rax                     # Move to the next byte in the source
+    testq %rdx, %rdx              # Check if we reversed all bytes in the inner loop
+    jnz reverse_bytes_HW1         # If not, continue reversing
 
     # Move to the next element
-    addq %rcx, %rsi              # Advance the source pointer by the size of the element
-    addq %rcx, %rdi              # Advance the destination pointer by the size of the element
-    decl %ebx                    # Decrease the element counter
-    jmp process_elements_HW1         # Repeat for the next element
+    addq %rcx, %rsi              # Advance the source pointer by the type
+    addq %rcx, %rdi              # Advance the destination pointer by the type
+    decl %ebx                    # Decrease the length
+    jmp main_loop_HW1            # Repeat for the next element
 
 done_HW1:
