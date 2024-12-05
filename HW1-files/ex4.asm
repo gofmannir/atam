0ba2f85dafd7fd40025e2149154568e868e234b5
gavanv <gavan777@gmail.com>
2024-12-05 07:32:37 +0000

    add tests

--- HW1-files/ex4.asm
+++ HW1-files/ex4.asm
@@ -1,5 +1,33 @@
 .global _start
 
+.section .data
+ Node1:
+    .quad 0
+    .int 15
+    .quad Node2
+
+ Node2:
+    .quad Node1
+    .int 15
+    .quad Node3
+
+Node4:
+    .quad Node3
+    .int 5
+    .quad 0
+
+Node3:
+    .quad 0
+    .int 5
+    .quad 0
+
+head:
+    .quad Node1
+
+node:
+    .quad 
+
+
 .section .text
 _start:
 #your code here
