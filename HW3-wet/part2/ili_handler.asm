7ef6cb2ce7344f864c93d0ba5f9b814069f185cd
Nir Gofman <gofmannir123@gmail.com>
2024-12-28 16:00:07 +0000

    finished part 2

--- HW3-wet/part2/ili_handler.asm
+++ HW3-wet/part2/ili_handler.asm
@@ -3,5 +3,112 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  ####### Some smart student's code here #######
-  iretq
+    # --- Save clobbered registers ---
+    pushq %r8
+    pushq %r9
+    pushq %r10
+    pushq %r11
+    pushq %r12
+    pushq %r13
+    pushq %r14
+    pushq %r15
+    pushq %rax
+    pushq %rbx
+    pushq %rcx
+    pushq %rdx
+    pushq %rsi
+    pushq %rbp
+    pushq %rsp
+
+	# get the opcode
+	movq 120(%rsp), %rax
+	movq (%rax), %rax
+    
+    cmpb $0x0F, %al        # Compare the first byte (AL) with 0x0F
+    je two_byte_opcode     # Jump if it starts with 0x0F (two-byte opcode)
+
+single_byte_opcode:
+    movq %rax, %rdi    # Pass RIP (pointer to opcode) to the function
+    call what_to_do    
+    
+    testl %eax, %eax
+    jz equal_zero
+
+    movq %rax, %rdi
+
+    # --- Restore registers ---
+    popq %rsp
+    popq %rbp
+    popq %rsi
+    popq %rdx
+    popq %rcx
+    popq %rbx
+    popq %rax
+    popq %r15
+    popq %r14
+    popq %r13
+    popq %r12
+    popq %r11
+    popq %r10
+    popq %r9
+    popq %r8
+
+    # do more logic
+    addq $1, (%rsp) # it holds the return adress, we go over it
+
+    iretq
+
+two_byte_opcode:
+    movb %ah, %al          # Shift the opcode to the right by 8 bits
+    movq %rax, %rdi        # Pass RIP (pointer to opcode) to the function
+    call what_to_do        # Call your C function
+    
+    testl %eax, %eax
+    jz equal_zero
+
+    movq %rax, %rdi
+
+    # --- Restore registers ---
+    popq %rsp
+    popq %rbp
+    popq %rsi
+    popq %rdx
+    popq %rcx
+    popq %rbx
+    popq %rax
+    popq %r15
+    popq %r14
+    popq %r13
+    popq %r12
+    popq %r11
+    popq %r10
+    popq %r9
+    popq %r8
+
+    # do more logic
+    addq $2, (%rsp) # it holds the return adress, we go over it
+
+    iretq
+
+equal_zero:
+    # --- Restore registers ---
+    popq %rsp
+    popq %rbp
+    popq %rsi
+    popq %r15
+    popq %r14
+    popq %r13
+    popq %r12
+    popq %r11
+    popq %r10
+    popq %r9
+    popq %r8
+    popq %rdx
+    popq %rcx
+    popq %rbx
+    popq %rax
+
+    jmpq *old_ili_handler
+
+    iretq
+    
--- HW3-wet/part2/ili_utils.c
+++ HW3-wet/part2/ili_utils.c
@@ -2,24 +2,36 @@
 
 void my_store_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
+    __asm__("sidt %0" : "=m"(*idtr));
 // </STUDENT FILL>
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
+    /*
+     * lidt (Load IDT Register) sets the CPU’s IDT pointer to whatever is
+     * specified in *idtr.
+     */
+    __asm__("lidt %0" : : "m"(*idtr));
 // <STUDENT FILL>
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
+    gate->offset_low    = (u16)(addr & 0xFFFF);
+    gate->offset_middle = (u16)((addr >> 16) & 0xFFFF);
+    gate->offset_high   = (u32)((addr >> 32) & 0xFFFFFFFF);
 // </STUDENT FILL>
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
+    unsigned long offset;
+    
+    offset  = (unsigned long)gate->offset_low;
+    offset |= (unsigned long)gate->offset_middle << 16;
+    offset |= (unsigned long)gate->offset_high   << 32;
+    
+    return offset;
 // </STUDENT FILL>
 }
